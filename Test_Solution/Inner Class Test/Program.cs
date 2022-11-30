22fefaaf31a275998ec9ada8ae19619a0c2ff84a
Junggyu <cjg17@naver.com>
2022-11-30 14:38:31 +0900

    Add InnerClass InnerEnum Test Code File[c#]

---
+++ Test_Solution/Inner Class Test/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// Class 안에 Class를 선언하는 방법
+// 이유 = Inner Class의 내용을 함부로 참조할 수 없게 하기 위한 수단
+
+namespace Inner_Class_Test
+{
+    class LocationKorea
+    {
+        private string seoulNumber = "02";
+
+        class Kangwon
+        {
+            void LocationNumber()
+            {
+                // 특정 Class 안에 선언된 Inner Class라고 연결되어 있는 구조 아님.
+                // 연관성 없는 각자 다른 Class로 인식되어 진행된다.
+
+                // 변수 seoulNumber는
+                // LocationKorea Class에 선언된 private 변수이기 때문에
+                // Kangwon Class에서는 참조할 수 없다.
+
+                // seoulNumber = "033";
+            }
+        }
+
+        class Jeju
+        {
+            // LocationKorea Class에 선언된 private 변수를 참조하기 위해서는
+            // Inner Class의 인자값으로 LocationKorea Class를 받아서
+            // seoulNumber 변수를 참조할 수 있다.
+
+            void LocationNumber(LocationKorea _locKr)
+            {
+                string jejuNumber = "064";
+                _locKr.seoulNumber = jejuNumber;
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Test_Solution/Inner Enum Test/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inner_Enum_Test
+{
+    class Player
+    {
+        // 특정 열거형 자료(enum)을 특정 Class에서만 사용할 때
+        // 해당 Class 안에서 선언하여 사용 하는 법
+        // 특징: 열거자 명의 중복성을 해결 할 수 있다.
+
+        enum PlayerJob
+        {
+            Programer,
+            Teacher,
+            Chief,
+            Doctor,
+            Repairman,
+            Police
+        }
+
+        PlayerJob manJob = PlayerJob.Doctor; 
+    }
+
+    class Arrow
+    {
+        public enum ArrowKind
+        {
+            Center,
+            Left,
+            Right,
+            Up,
+            Down
+        }
+
+        ArrowKind arrowLeft = ArrowKind.Left;
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player newPlayer = new Player();
+            Arrow newArrow = new Arrow();
+
+            // 다른 Class에서 Inner Enum 데이터를 참조하고 싶을 때
+            Arrow.ArrowKind arrowLeft = Arrow.ArrowKind.Left;
+
+            // 'public' 으로 선언 된 열거자(Enum)만 참조하여 사용할 수 있다.
+            //Player.PlayerJob chief = Player.PlayerJob.Chief;
+        }
+    }
+}
